329fbd1b59fcdb935656d9cc638e51aee28d247d
josh-segal <122911717+josh-segal@users.noreply.github.com>
2024-04-03 20:52:52 -0400

    check-in

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import os
 from bs4 import BeautifulSoup
 import re
 import nltk
@@ -5,6 +6,7 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
 def read_file(file_path):
@@ -104,3 +106,87 @@ def preprocess_text(text_data):
         # Join tokens back to text
         preprocessed_text.append(' '.join(words))
     return preprocessed_text
+
+
+def get_prfa(dev_y: list, preds: list, verbose=False) -> tuple:
+    """
+    Calculate precision, recall, f1, and accuracy for a given set of predictions and labels.
+    Args:
+        dev_y: list of labels
+        preds: list of predictions
+        verbose: whether to print the metrics
+    Returns:
+        tuple of precision, recall, f1, and accuracy
+    """
+    precision = precision_score(dev_y, preds)
+    recall = recall_score(dev_y, preds)
+    accuracy = accuracy_score(dev_y, preds)
+    f1 = f1_score(dev_y, preds)
+    # print("f1:", f1)
+
+    return precision, recall, f1, accuracy
+
+
+def create_presentation_to_paper_mapping(presentation_folder, paper_folder):
+    presentation_to_paper = {}
+    presentation_files = sorted(os.listdir(presentation_folder))
+    paper_files = sorted(os.listdir(paper_folder))
+
+    # Assuming presentation and paper files have the same indexes
+    for idx, (presentation_file, paper_file) in enumerate(zip(presentation_files, paper_files)):
+        presentation_index = presentation_file.split('.')[0]  # Extract index from presentation filename
+        paper_index = paper_file.split('.')[0]  # Extract index from paper filename
+        assert presentation_index == paper_index, "Mismatch between presentation and paper indexes"
+        presentation_to_paper[presentation_index] = idx  # Store index instead of paper name
+
+    return presentation_to_paper
+
+
+def process_papers_folder(folder_path):
+    """
+    Process files in a folder using a given parse function.
+
+    Args:
+    - folder_path (str): Path to the folder containing files.
+    - parse_function (function): Function to parse the content of each file.
+
+    Returns:
+    - all_data (list): List of processed data from all files in the folder.
+    """
+    all_data = []
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):
+            # Read the file content
+            file_content = read_file(file_path)
+            if file_content is not None:
+                # Parse the content using the provided parse function
+                parsed_data = parse_paper_xml(file_content)
+                if parsed_data:
+                    all_data.extend(parsed_data)
+    return all_data
+
+
+def process_presentation_folder(folder_path):
+    """
+    Process files in a folder using a given parse function.
+
+    Args:
+    - folder_path (str): Path to the folder containing files.
+    - parse_function (function): Function to parse the content of each file.
+
+    Returns:
+    - all_data (list): List of processed data from all files in the folder.
+    """
+    all_data = []
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):
+            # Read the file content
+            file_content = read_file(file_path)
+            if file_content is not None:
+                # Parse the content using the provided parse function
+                parsed_data = parse_presentation_xml(file_content)
+                if parsed_data:
+                    all_data.extend(parsed_data)
+    return all_data
